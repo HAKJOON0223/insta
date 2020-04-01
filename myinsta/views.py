7f790cdf7362997c5f9a9ad2679b064e6b3bf5a7
HAKJOON0223 <2hakjoon@gmail.com>
2020-04-02 00:22:53 +0900

    template added!

--- myinsta/views.py
+++ myinsta/views.py
@@ -1,4 +1,6 @@
 from django.views import generic
+from .models import Post
 
-class IndexView(generic.Listview):
-    template_name = ''
+class IndexView(generic.ListView):
+    template_name = 'insta/index.html'
+    model = Post
