343e017ade2b72b9df0bd307c6d5173999fcee15
HAKJOON0223 <2hakjoon@gmail.com>
2020-04-01 23:38:25 +0900

    Navbar_added!

--- insta/urls.py
+++ insta/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', admin.site.urls),
-    path('insta.', include('myinsta.urls')),
+    path('insta/', include('myinsta.urls')),
 ]
