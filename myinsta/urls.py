cb60f4fcb036d1a20e5aebb0daff5ec4b6a22c58
HAKJOON0223 <2hakjoon@gmail.com>
2020-04-02 22:00:06 +0900

    Adding Posts

--- myinsta/urls.py
+++ myinsta/urls.py
@@ -20,4 +20,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view()),
+    path('EditPost/', views.EditPost, name = 'EditPost'),
 ]
--- myinsta/views.py
+++ myinsta/views.py
@@ -1,6 +1,12 @@
-from django.views import generic
+from django.views.generic import ListView, DetailView
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import render
 
-class IndexView(generic.ListView):
+class IndexView(ListView):
     template_name = 'insta/index.html'
     model = Post
+
+def EditPost(request):
+    form = PostForm()
+    return render(request, 'insta/EditPost.html', {'form': form})
