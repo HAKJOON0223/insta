28abf9afe45bac9acd4604c52043eda56e21f520
HAKJOON0223 <2hakjoon@gmail.com>
2020-04-03 23:44:57 +0900

    when I post posts created_date -> published_date

--- myinsta/models.py
+++ myinsta/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from imagekit.models import ProcessedImageField
-from imagekit.processors import Thumbnail
+from imagekit.processors import Thumbnail, ResizeToFill
 
 
 class Post(models.Model):
@@ -11,14 +11,14 @@ class Post(models.Model):
     text = models.TextField()
     photo = ProcessedImageField(
             upload_to = 'photo/',
-            processors = [Thumbnail(600,600)],
+            processors = [ResizeToFill(600,600)],
             format = 'JPEG',
             options = {'quality': 60}
     )
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
-            blank=True, null=True)
+            default=timezone.now)
 
     def __str__(self):
         return self.title
