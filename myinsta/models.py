ef3277deb18aff71e070374d18332e0adb9e694f
HAKJOON0223 <2hakjoon@gmail.com>
2020-04-03 23:34:41 +0900

    processedImage function added

--- myinsta/models.py
+++ myinsta/models.py
@@ -1,13 +1,20 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from imagekit.models import ProcessedImageField
+from imagekit.processors import Thumbnail
 
 
 class Post(models.Model):
     author = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    photo = models.ImageField(upload_to="")
+    photo = ProcessedImageField(
+            upload_to = 'photo/',
+            processors = [Thumbnail(600,600)],
+            format = 'JPEG',
+            options = {'quality': 60}
+    )
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
